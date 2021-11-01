0d3399278e1ba3c84d3112ab61c272901da99c0f
O Teerasak Vichadee <iolumin@gmail.com>
2021-11-02 06:39:28 +0700

    update using ipfs to store image

--- scripts/run.js
+++ scripts/run.js
@@ -3,9 +3,9 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ["Leo", "Aang", "Pikachu"],
     [
-      "https://i.imgur.com/pKd5Sdk.png",
-      "https://i.imgur.com/xVu4vFL.png",
-      "https://i.imgur.com/WMB6g9u.png",
+      "bafybeibsifcmwkufr7zwh5s3ekvjkfj5nnadjhweniz4p7lxqelt7mbp74",
+      "bafybeibsifcmwkufr7zwh5s3ekvjkfj5nnadjhweniz4p7lxqelt7mbp74",
+      "bafybeibsifcmwkufr7zwh5s3ekvjkfj5nnadjhweniz4p7lxqelt7mbp74",
     ],
     [100, 200, 300],
     [100, 50, 25],
